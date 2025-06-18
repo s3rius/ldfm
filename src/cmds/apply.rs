8327519cde16f2233317cfd520d33b4660381936
Pavel Kirilin <win10@list.ru>
2025-06-18 15:34:23 +0200

    Added apply command.

--- src/cmds/apply.rs
+++ src/cmds/apply.rs
@@ -1,12 +1,22 @@
+use fs_extra::dir::CopyOptions;
+
 use crate::configs::LdfmConfig;
 
 pub fn run(config: LdfmConfig) -> anyhow::Result<()> {
     let repo_config = config.get_repo_config()?;
     for (key, value) in repo_config.files.iter() {
-        let Some(full_path) = simple_expand_tilde::expand_tilde(value) else {
+        let Some(to_path) = simple_expand_tilde::expand_tilde(value) else {
             continue;
         };
-        tracing::info!("{}: {:?}", key, full_path);
+        let from_path = config.local_path.join(repo_config.get_local_path(key));
+        tracing::info!("Copying {} -> {}", from_path.display(), to_path.display());
+        fs_extra::copy_items(
+            &[from_path],
+            to_path
+                .parent()
+                .ok_or(anyhow::anyhow!("Cannot get a parent directory."))?,
+            &CopyOptions::new().overwrite(true).copy_inside(true),
+        )?;
     }
     Ok(())
 }
